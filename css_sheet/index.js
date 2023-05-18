7867d7a7bf65f51c8d69fe1c04c69606df0a1e3f
Red <ahmad.faezi420@gmail.com>
2023-05-18 19:03:20 +0200

    added a debounce funtion with the help of gpt thanks almighty gpt

--- css_sheet/index.js
+++ css_sheet/index.js
@@ -1,41 +1,39 @@
-// window.addEventListener('scroll', function() {
-//     const header = document.querySelector('header');
-//     const body = document.querySelector('body');
-//     if (window.pageYOffset > 0) {
-//       header.classList.add('sticky');
-//       body.classList.add('sticky_nav');
-//     } else {
-//       header.classList.remove('sticky');
-//       body.classList.remove('sticky_nav');
-//     }
-//   });
-// let isScrolling = false;
+// Debounce function to delay the scroll event handler
+function debounce(func, delay) {
+  let timer;
+  return function() {
+    clearTimeout(timer);
+    timer = setTimeout(func, delay);
+  };
+}
 
-// window.addEventListener('scroll', function() {
-//   const header = document.querySelector('header');
-//   const body = document.querySelector('body');
-//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   const target = scrollTop > 0 && !body.classList.contains('sticky_nav') ? header.offsetTop : 0;
-//   const duration = 500; // Adjust the duration (in milliseconds) to your liking
-  
-//   if (scrollTop > 0) {
-//     header.classList.add('sticky');
-//     body.classList.add('sticky_nav');
-//   } else {
-//     header.classList.remove('sticky');
-//     body.classList.remove('sticky_nav');
-//   }
-  
-//   if (!isScrolling && scrollTop !== target) {
-//     isScrolling = true;
-//     scrollTo({
-//       top: target,
-//       behavior: 'smooth',
-//       duration: duration
-//     });
-    
-//     setTimeout(function() {
-//       isScrolling = false;
-//     }, duration);
-//   }
-// });
+let isScrolling = false;
+
+window.addEventListener('scroll', debounce(function() {
+  const header = document.querySelector('header');
+  const body = document.querySelector('body');
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const target = scrollTop > 0 && !body.classList.contains('sticky_nav') ? header.offsetTop : 0;
+  const duration = 500; // Adjust the duration (in milliseconds) to your liking
+
+  if (scrollTop > 0) {
+    header.classList.add('sticky');
+    body.classList.add('sticky_nav');
+  } else {
+    header.classList.remove('sticky');
+    body.classList.remove('sticky_nav');
+  }
+
+  if (!isScrolling && scrollTop !== target) {
+    isScrolling = true;
+    scrollTo({
+      top: target,
+      behavior: 'smooth',
+      duration: duration
+    });
+
+    setTimeout(function() {
+      isScrolling = false;
+    }, duration);
+  }
+}), 50);
